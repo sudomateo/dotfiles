15649020f6d3f8860535b8331a27e3ddacb5cc87
Matthew Sanabria <me@matthewsanabria.dev>
2025-06-10 20:34:33 -0400

    fish: update environment variables and path

--- fish/.config/fish/conf.d/sudomateo.fish
+++ fish/.config/fish/conf.d/sudomateo.fish
@@ -7,22 +7,21 @@ set --global --export CARGO_HOME ~/.local/cargo
 # Go.
 set --global --export GOPATH ~/.local/gopath
 
-# CockroachDB.
-set --global --export COCKROACH_AUTO_BALLAST false
-
 # Editors.
-set --global --export VISUAL hx
-set --global --export EDITOR hx
+if type --no-functions hx
+    set --global --export VISUAL hx
+    set --global --export EDITOR hx
+end
 
-set --global --export HELIX_RUNTIME ~/personal/helix/runtime
-set --global --export SSH_AUTH_SOCK ~/.1password/agent.sock
+set --local 1password_ssh_socket ~/.1password/agent.sock
+if test -e $1password_ssh_socket -a -S $1password_ssh_socket
+    set --global --export SSH_AUTH_SOCK $1password_ssh_socket
+end
 
 fish_add_path --global \
+    ~/.local/bin \
+    $CARGO_HOME/bin \
     $GOPATH/bin \
     ~/.local/go/bin \
     ~/.local/zig \
-    $CARGO_HOME/bin \
-    ~/.local/bin \
     ~/bin
-
-set PATH bin $PATH
